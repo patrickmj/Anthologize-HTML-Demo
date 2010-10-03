06b67b81efb2c5c9e565fdfd50a2ba39b3a2a18d
Patrick Murray-John <pgosetti@umw.edu>
2010-10-03 16:23:17 -0400

    add gravatars, project titles, and more More MORE

--- output.php
+++ output.php
@@ -5,11 +5,14 @@ include_once(ANTHOLOGIZE_TEIDOMAPI_PATH);
 require_once(WP_PLUGIN_DIR. '/anthologize/includes/theming-functions.php');
 global $api;
 
+
+//TODO: simplify and condense these options
+
 $ops = array('includeStructuredSubjects' => false, //Include structured data about tags and categories
 		'includeItemSubjects' => false, // Include basic data about tags and categories
 		'includeCreatorData' => true, // Include basic data about creators
-		'includeStructuredCreatorData' => false, //include structured data about creators
-		'includeOriginalPostData' => false, //include data about the original post (true to use tags and categories)
+		'includeStructuredCreatorData' => true, //include structured data about creators
+		'includeOriginalPostData' => true, //include data about the original post (true to use tags and categories and creator data)
 		'checkImgSrcs' => true, //whether to check availability of image sources
 		'linkToEmbeddedObjects' => false,
 		'indexSubjects' => false,
@@ -26,7 +29,6 @@ $tei = new TeiDom($_SESSION, $ops);
 $api = new TeiApi($tei);
 
 
-
 $fileName = $api->getFileName();
 $ext = "html";
 
@@ -40,10 +42,12 @@ if( isset($ops['outputParams']['download']) ) {
 
 <html>
 	<head>
-		<title>testing</title>
+		<title><?php anth_project_title(true); ?></title>
 	</head>
 
 	<body>
+	<h1><?php anth_project_title(); ?></h1>
+
 	<?php
 
 	anth_the_section('body');
@@ -63,13 +67,16 @@ if( isset($ops['outputParams']['download']) ) {
 
 				?>
 				<h3><?php anth_the_title() ?></h3>
+				<div class="item-meta">
 
+					<img class="gravatar" src="<?php anth_author_gravatar(); ?>" />
+					<span class="item-author">By <?php anth_the_author() ?></span>
+
+				</div>
 				<div class="item-content">
 					<?php anth_item_content() ?>
 				</div>
-				<div class="item-meta">
-					<span class="item-author">By <?php anth_the_author() ?></span>
-				</div>
+
 				<?php
 			}
 		}
